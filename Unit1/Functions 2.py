283404eacdc0ac44fbf0becee166c8b0c21d11c9
conno <22816@stpauls.qld.edu.au>
2022-02-16 13:42:21 +1000

    Connor L

--- Unit1/Functions 2.py
+++ Unit1/Functions 2.py
@@ -1,33 +1,26 @@
+v = input("Choose an Operation [m/d/a/s]: ")
 # create a function that takes 2 numbers and MULTIPLIES them, returning the result
 def multiply(num1, num2):
-    print(num1 * num2)
+    if v == "m":
+        print(num1 * num2)
 
 # create a function that takes 2 numbers and DIVIDES the second from the first, returning the result
 def divide(num1, num2):
-    print(num1 / num2)
+    if v == "d":
+        print(num1 / num2)
 
 
 # create a function that takes 2 numbers and ADDS them, returning the result
 def add(num1, num2):
-    print(num1 + num2)
+    if v == "a":
+        print(num1 + num2)
 
 # create a function that takes 2 numbers and MINUSES the second from the first, returning the result
 def subtract(num1, num2):
-    print(num1 - num2)
+    if v == "s":
+        print(num1 - num2)
 
 multiply(20, 5)
 divide(1000, 15)
 add(65, 25)
 subtract(147, 47)
-
-variable1 = input("Choose an Operation [m/d/a/s]: ")
-
-if variable1 == "m":
-    multiply(num1, num2)
-if variable2 == "d":
-    divide(num1, num2)
-if variable3 == "a":
-    add(num1, num2)
-if variable4 == "s":
-    subtract(num1, num2)
-
