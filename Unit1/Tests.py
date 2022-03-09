aac13a5420655f61d2201819c208e5bc09222a66
conno <22816@stpauls.qld.edu.au>
2022-03-09 12:39:15 +1000

    Connor L

---
+++ Unit1/Tests.py
@@ -0,0 +1,43 @@
+# a = 5
+# b = 2
+# c = 3
+#
+# result = a + b * c
+# print(result)
+
+# num = 99
+# num = 5
+# print(num)
+
+# If the following PSEUDOCODE were an actual program, why would it not display the output that the programmer expects?
+
+# fave_food - assignment error
+# DECLARE String favouriteFood
+# favouritefood = (input("What is the name of your favourite food?: "))
+# print(f"Your favourite food is {favouritefood}")
+#
+# DECLARE favouritefood = INPUT
+# DISPLAY INPUT
+
+# first_prize - Assignment error
+
+# FirstPrize = (input("Enter the award for first prize: "))
+# print("The first prize winner will receive",FirstPrize)
+
+# average_score - Arithmitic error
+# DECLARE Integer lowest, highest, average
+#
+# low = int(input("Enter the lowest score: "))
+# high = int(input("Enter the highest score: "))
+# average = (low + high) / 2
+# print(f"The average is {average}")
+#
+# # Room_length - sequence
+# length = input("Enter the length of the room: ")
+
+
+print("Connor")
+print("Brisbane, Queensland, 4034")
+print("0401734007")
+print("Composer")
+
